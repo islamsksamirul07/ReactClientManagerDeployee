59cb75297a0a9875539b4f305ae4dd0aff0888f5
islamsksamirul07 <117712542+islamsksamirul07@users.noreply.github.com>
2025-01-05 16:37:34 +0530

    correct url

--- src/component/base_url_api.js
+++ src/component/base_url_api.js
@@ -2,11 +2,11 @@ import React from "react";
 
 // const base_url="http://localhost:8082/client";
 // This is for client
-const base_url="https://clienttest-oxlc.onrender.com";
+const base_url="https://clienttest-oxlc.onrender.com/client";
 // const base_url="http://13.49.167.242:8090";
 
 // this url for manager
-const base_url1=`https://spring-manager-allupdated.onrender.com`;
+const base_url1=`https://reactclientmanagerdeployee.onrender.com/manager`;
 
 // export default {base_url,base_url1};
 const urls = { base_url, base_url1 };
